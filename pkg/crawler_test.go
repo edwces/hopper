61bf68e8914c4800c954548fd82ff1129424f78c
edwces <68016750+edwces@users.noreply.github.com>
2023-05-29 13:34:01 +0200

    refactor testing

--- pkg/crawler_test.go
+++ pkg/crawler_test.go
@@ -13,7 +13,7 @@ const DefaultTestDelay = time.Millisecond * 100
 
 func NewMockServer(port int) (*httptest.Server, error) {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/main", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
 		w.Write([]byte(`<html>
 							<head>
@@ -25,6 +25,18 @@ func NewMockServer(port int) (*httptest.Server, error) {
 								<a href="/link2"></a>
 								<a href="#"></a>
 								<a href="javascript:void(0)"></a>
+							</body>
+						</html>`))
+	})
+	mux.HandleFunc("/mime", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte(`<html>
+							<head>
+								<title>Test Page</title>
+							</head>
+							<body>
+								<h1>hsdhdjhshjdh</h1>
+								<a href="/link2"></a>
 							</body>
 						</html>`))
 	})
@@ -49,11 +61,11 @@ func NewMockServer(port int) (*httptest.Server, error) {
 							<body>
 								<h1>hsdhdjhshjdh</h1>
 								<a href="/link1"></a>
-								<a href="/mime"></a>
+								<a href="/link3"></a>
 							</body>
 						</html>`))
 	})
-	mux.HandleFunc("/mime", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/link3", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain")
 		fmt.Fprint(w, "Hello Mars")
 	})
@@ -139,7 +151,7 @@ func TestCrawl(t *testing.T) {
 
 	crawler := Crawler{Delay: DefaultTestDelay}
 	crawler.Init()
-	results := crawler.Crawl("http://127.0.0.1:8080/")
+	results := crawler.Crawl("http://127.0.0.1:8080/main")
 	if len(results) != 3 {
 		t.Errorf("Incorrect length of results: got %d, expected: %d", len(results), 3)
 	}
@@ -155,7 +167,7 @@ func TestCrawlMime(t *testing.T) {
 
 	crawler := Crawler{Mediatype: "text/plain", Delay: DefaultTestDelay}
 	crawler.Init()
-	results := crawler.Crawl("http://127.0.0.1:8080/")
+	results := crawler.Crawl("http://127.0.0.1:8080/mime")
 	if len(results) != 1 {
 		t.Errorf("Incorrect length of results: got %d, expected: %d", len(results), 1)
 	}
@@ -222,24 +234,9 @@ func TestCrawlRedirect(t *testing.T) {
 func TestCrawlRequestUserAgent(t *testing.T) {
 	crawler := Crawler{Delay: DefaultTestDelay}
 	crawler.Init()
-	req := crawler.newRequest("GET", "http://127.0.0.1:8080/")
+	req := crawler.newRequest("GET", "http://127.0.0.1:8080/main")
 	userAgent := req.Header.Get("User-Agent")
 	if userAgent != DefaultUserAgent {
 		t.Errorf("Incorrect user-agent header set: got %s, expected %s", userAgent, DefaultUserAgent)
-	}
-}
-
-func BenchmarkCrawl(b *testing.B) {
-	server, err := NewMockServer(8080)
-	if err != nil {
-		b.Fatalf("Server could not be started")
-	}
-	server.Start()
-	defer server.Close()
-
-	for i := 0; i < b.N; i++ {
-		crawler := Crawler{}
-		crawler.Init()
-		crawler.Crawl("http://127.0.0.1:8080/")
 	}
 }
