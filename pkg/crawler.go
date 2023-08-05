6b359bd5a13e0695a1e095fb1224b1c85c2d2306
edwces <68016750+edwces@users.noreply.github.com>
2023-08-05 13:00:28 +0200

    Crawler callback OnParse

--- pkg/crawler.go
+++ pkg/crawler.go
@@ -1,7 +1,6 @@
 package hopper
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -11,75 +10,82 @@ import (
 )
 
 type Crawler struct {
-	queue     []*url.URL
-	seen      map[*url.URL]bool
+	OnParse func(*http.Response, *html.Node)
+
+	queue []*url.URL
+	seen  map[*url.URL]bool
 }
 
 // Init initializes default values for crawler.
 func (c *Crawler) Init() {
-    c.queue = []*url.URL{}
-    c.seen = map[*url.URL]bool{}
+    if c.OnParse == nil {
+        c.OnParse = func(r *http.Response, n *html.Node) {}
+    }
+
+	c.queue = []*url.URL{}
+	c.seen = map[*url.URL]bool{}
 }
 
 // Traverse uses depth-first search for link traversal.
 func (c *Crawler) Traverse(seeds ...string) {
 	for _, seed := range seeds {
 		if uri, err := url.Parse(seed); err == nil {
-        	c.seen[uri] = true
-		    c.queue = append(c.queue, uri)
-        }
+			c.seen[uri] = true
+			c.queue = append(c.queue, uri)
+		}
 	}
 
 	for len(c.queue) != 0 {
-        uri := c.queue[len(c.queue)-1]
-        c.queue = c.queue[:len(c.queue)-1]
+		uri := c.queue[len(c.queue)-1]
+		c.queue = c.queue[:len(c.queue)-1]
 		c.Visit(uri)
 	}
 }
 
 func (c *Crawler) Visit(uri *url.URL) {
-    fmt.Println("URL: ", uri.String())
-    resp, err := http.Get(uri.String())
+	resp, err := http.Get(uri.String())
 	if resp != nil {
 		defer resp.Body.Close()
 	}
 	if err != nil {
-		return 
+		return
 	}
 
 	bytes, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return 
+		return
 	}
 	doc, err := html.Parse(strings.NewReader(string(bytes)))
 	if err != nil {
-		return 
+		return
 	}
-    
-    // Extract uri's from document.
-    var f func(*html.Node)
-    f = func(n *html.Node) {
-        if n.Type == html.ElementNode && n.Data == "a" {
-            for _, attr := range n.Attr {
-                if attr.Key == "href" {
-                    discovery, err := url.Parse(attr.Val)
-                    if err != nil {
-                        continue
-                    }
-                    resolved := uri.ResolveReference(discovery)
-                    if c.seen[resolved] || !validURI(resolved) {
-                        continue
-                    }
-                    c.seen[resolved] = true
-		            c.queue = append(c.queue, discovery)
-                }
-                }
-            }
-        for ch := n.FirstChild; ch != nil; ch = ch.NextSibling {
-            f(ch)
-        }
-    }
-    f(doc)
+
+	c.OnParse(resp, doc)
+
+	// Extract uri's from document.
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, attr := range n.Attr {
+				if attr.Key == "href" {
+					discovery, err := url.Parse(attr.Val)
+					if err != nil {
+						continue
+					}
+					resolved := uri.ResolveReference(discovery)
+					if c.seen[resolved] || !validURI(resolved) {
+						continue
+					}
+					c.seen[resolved] = true
+					c.queue = append(c.queue, resolved)
+				}
+			}
+		}
+		for ch := n.FirstChild; ch != nil; ch = ch.NextSibling {
+			f(ch)
+		}
+	}
+	f(doc)
 }
 
 func validURI(uri *url.URL) bool {
