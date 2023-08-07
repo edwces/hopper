5683740a779e51c5dbe455e5a550cd8915e5ea92
edwces <68016750+edwces@users.noreply.github.com>
2023-08-07 09:54:33 +0200

    concurrency safe URLQueue

--- pkg/queue.go
+++ pkg/queue.go
@@ -1,8 +1,12 @@
 package hopper
 
-import "net/url"
+import (
+	"net/url"
+	"sync"
+)
 
 type URLQueue struct {
+    sync.Mutex
 	queue []*url.URL
 	seen  map[string]bool
 }
@@ -15,6 +19,9 @@ func NewURLQueue() *URLQueue {
 }
 
 func (u *URLQueue) Push(uri *url.URL) {
+    u.Lock()
+    defer u.Unlock()
+
 	if !u.seen[uri.String()] {
 		u.seen[uri.String()] = true
 		u.queue = append(u.queue, uri)
@@ -22,11 +29,17 @@ func (u *URLQueue) Push(uri *url.URL) {
 }
 
 func (u *URLQueue) Pop() *url.URL {
+    u.Lock()
+    defer u.Unlock()
+
 	uri := u.queue[len(u.queue)-1]
 	u.queue = u.queue[:len(u.queue)-1]
 	return uri
 }
 
 func (u *URLQueue) Length() int {
+    u.Lock()
+    defer u.Unlock()
+
 	return len(u.queue)
 }
