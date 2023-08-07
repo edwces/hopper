f11ad1824a7323bfe4336251d0e899293d8bc7fb
edwces <68016750+edwces@users.noreply.github.com>
2023-08-07 17:09:35 +0200

    Move worker to Crawler struct

--- pkg/crawler.go
+++ pkg/crawler.go
@@ -1,35 +1,41 @@
 package hopper
 
 import (
+	"io"
 	"net/http"
 	"net/url"
 	"runtime"
+	"strings"
+	"time"
 
 	"golang.org/x/net/html"
 )
 
 const DefaultUserAgent = "hopper/0.1"
 
+// NOTE: might be better to refactor Worker proccess into it's own class
+// but this way we would also need some easy way to copy config
+
 type Crawler struct {
-    UserAgent string
-	OnParse func(*http.Response, *html.Node)
-    Threads int
+	UserAgent string
+	OnParse   func(*http.Response, *html.Node)
+	Threads   int
 
 	queue *URLQueue
 }
 
 // Init initializes default values for crawler.
 func (c *Crawler) Init() {
-    if c.UserAgent == "" {
-        c.UserAgent = DefaultUserAgent
-    }
-    if c.Threads == 0 {
-        c.Threads = runtime.GOMAXPROCS(0)
-    }
+	if c.UserAgent == "" {
+		c.UserAgent = DefaultUserAgent
+	}
+	if c.Threads == 0 {
+		c.Threads = runtime.GOMAXPROCS(0)
+	}
 
-    if c.OnParse == nil {
-        c.OnParse = func(r *http.Response, n *html.Node) {}
-    }
+	if c.OnParse == nil {
+		c.OnParse = func(r *http.Response, n *html.Node) {}
+	}
 
 	c.queue = NewURLQueue(c.Threads)
 }
@@ -38,24 +44,96 @@ func (c *Crawler) Init() {
 func (c *Crawler) Run(seeds ...string) {
 	for _, seed := range seeds {
 		if uri, err := url.Parse(seed); err == nil {
-             go c.queue.Push(uri)
+			go c.queue.Push(uri)
 		}
 	}
-    
-    // For each free place in queue create new Worker
-    for free := range c.queue.Free {
-        for i := 0; i < free; i++ {
-            go c.Work()
-        }
-    }
+
+	// For each free place in queue create new Worker
+	for free := range c.queue.Free {
+		for i := 0; i < free; i++ {
+			go c.StartNewWorker()
+		}
+	}
 }
 
-// NOTE: Figure some way to not copy all of config every time
-func (c *Crawler) Work() {
-    c.queue.AddThread()
-    work := Worker{queue: c.queue, OnParse: c.OnParse, UserAgent: c.UserAgent}
-    work.Traverse()
-    c.queue.RemoveThread()
+func (c *Crawler) StartNewWorker() {
+	c.queue.AddThread()
+	c.Traverse()
+	c.queue.RemoveThread()
 }
 
+func (c *Crawler) Traverse() {
+	for c.queue.Length() != 0 {
+		c.Visit(c.queue.Pop())
+	}
+}
 
+func (c *Crawler) Visit(uri *url.URL) {
+	res, err := c.Request(uri.String())
+	// temporarily FOR safety
+	time.Sleep(time.Second * 5)
+
+	if res != nil {
+		defer res.Body.Close()
+	}
+	if err != nil {
+		return
+	}
+
+	bytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+	doc, err := html.Parse(strings.NewReader(string(bytes)))
+	if err != nil {
+		return
+	}
+
+	c.OnParse(res, doc)
+
+	// Extract uri's from document.
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, attr := range n.Attr {
+				if attr.Key == "href" {
+					discovery, err := url.Parse(attr.Val)
+					if err != nil {
+						continue
+					}
+					resolved := uri.ResolveReference(discovery)
+					if !validURI(resolved) {
+						continue
+					}
+
+					c.queue.Push(resolved)
+
+				}
+			}
+		}
+		for ch := n.FirstChild; ch != nil; ch = ch.NextSibling {
+			f(ch)
+		}
+	}
+	f(doc)
+}
+
+func (c *Crawler) Request(uri string) (*http.Response, error) {
+	req, err := http.NewRequest("GET", uri, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", c.UserAgent)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return res, err
+	}
+	return res, nil
+}
+
+func validURI(uri *url.URL) bool {
+	if uri.Scheme != "http" && uri.Scheme != "https" {
+		return false
+	}
+	return true
+}
